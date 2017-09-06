326cd97d92d6bff8c9e3c8b5475529de6c5135f5
MikeTa <MikeTa@MikeTa-LT001.datacom.net.nz>
2017-09-06 16:05:38 +1200

    Add invalid test

--- src/OperationalDataManual/ODMTests.java
+++ src/OperationalDataManual/ODMTests.java
@@ -142,4 +142,11 @@ public class ODMTests {
         assertTrue("Fuel data provided processed", fuelData.getValidData());
     }
     
+    @Test
+    public void rejectInvalidDataLine () {
+        String dataStr = "|32000.0| 400.0| 9000.0| 7.3|23.2|";
+        FuelRateData fuelData = new FuelRateData(dataStr);
+        
+        assertFalse("Incorrect fuel data rejected", fuelData.getValidData());
+    }
 }
